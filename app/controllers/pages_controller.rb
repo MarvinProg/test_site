91e8c0bc59f69b09c57f3ca832525241e001377a
MarvinProg <85562528+MarvinProg@users.noreply.github.com>
2021-07-13 12:25:48 +0300

    Add model pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,17 @@
 class PagesController < ApplicationController
+
+	before_action :set_page, only: [:show, :edit, :update, :destroy]
+	before_action :set_new_page, only: [:new, :create]
+
+	def index
+		@pages ||= Page.roots.map do |page|
+			page.subtree.arrange
+		end
+	end
+
+	def show
+		@page = @page.subtree.arrange
+	end
+
+	
 end
