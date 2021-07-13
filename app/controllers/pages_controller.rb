f56b466a8d8cbae8c18a0abc27cd9b75fd1607e7
MarvinProg <85562528+MarvinProg@users.noreply.github.com>
2021-07-13 13:31:21 +0300

    Write method show, create, update, destroy

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,5 +13,49 @@ class PagesController < ApplicationController
 		@page = @page.subtree.arrange
 	end
 
+	def new
 	
+	end
+
+	def edit 
+
+	end
+
+	def create
+		@page.assing_attributes(page_params)
+
+		respond_to do |format|
+			if @page.save
+				format.html { redirect_to @page, notice: 'Page was created.'}
+				format.json { render :show, status: :created, location: @page }
+			else
+				format.html { render :new }
+				format.json { render json: @page.errors, status: :unprocessable_entity }
+			end
+		end
+	end
+
+	def update
+		respond_to do |format|
+			if @page.update(page_params)
+				format.html { redirect_to @page, notice: 'Page was update'}
+				format.json { render :show, status: :ok, location: @page }
+			else
+				format.html { render :edit }
+				format.json { render :json @page.errors, status: :unprocessable_entity }
+			end
+		end
+	end
+
+	def destroy
+		@page.destroy
+		redirect_url = @page.is_root? ? pages_url(@pages.parent)
+		respond_to do |format|
+			format.html { redirect_to redirect_url, notice: 'Page was destoyed.'}
+			format.json { head :no_content }
+		end
+	end
+
+	
+
 end
