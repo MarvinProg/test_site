ef1cd4ebbb267247703e9b2e0ff3bd9d290fca16
MarvinProg <85562528+MarvinProg@users.noreply.github.com>
2021-07-13 15:44:55 +0300

    Сheck validation and filling on page

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,25 @@
 module PagesHelper
+
+=begin
+def html_to_markdown(with_html)
+    with_html.to_s
+      .gsub(/<b>(.+?)<\/b>/) { "**#{$1}**" }
+      .gsub(/<i>(.+?)<\/i>/) { "\\\\#{$1}\\\\" }
+      .gsub(/<a href\=https?:\/\/.+?\/(.+?)>(.*?)<\/a>/) do
+        "((#{$1}[#{$2}]))"
+      end
+  end
+
+  def markdown_to_html(val, url=nil)
+    val.to_s
+      .gsub(/\*{2}(.+?)\*{2}/) { "<b>#{$1}</b>" }
+      .gsub(/\\{2}(.+?)\\{2}/) { "<i>#{$1}</i>" }
+      .gsub(/\({2}(.*?)\[(.+?)\]\){2}/) do
+        href = url ? "#{url}/#{$1}" : $1
+        "<a href=#{href}>#{$2}</a>"
+      end
+  end
+
+=end
+
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,41 @@
 class Page < ApplicationRecord
+
+	has_ancestry
+
+	validates_presence_of :name, :title, :text
+	validates :name,
+		format: { with: /\A[\wа-я]+\z/i }
+		uniqueness: true
+
+	def to_param
+		@to_param ||= Page.where(id: path_ids)
+			.order(:created_at)
+			.pluck(:name)
+			.join('/')
+	end
+
+	def self.find_by_path(path)
+		names = path.to_s.split('/')
+		ids = Page.where(name: names).pluck(:id)
+		pages = Page.find_by(name: names.last)
+
+		if page.present? && page.path_ids.to_set == ids.to_set
+			page
+		else
+			raise ActiveRecord::RecordNotFound
+		end
+	end
+
+	def self.new_by_path(path)
+		return Page.new unless path.present?
+
+		parent = Page.find_by_path(path)
+
+		if parent.present?
+			parent.children.new
+		else 
+			raise ActiveRecord::RecordNotFound
+		end
+	end
+
 end
